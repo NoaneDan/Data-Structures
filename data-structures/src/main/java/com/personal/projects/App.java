01867b76f559a5e07142fd312a0a1406c3c3cfbd
Noane Dan <Noane.Dan@outlook.com>
2019-01-08 22:51:51 +0200

    App

---
+++ data-structures/src/main/java/com/personal/projects/App.java
@@ -0,0 +1,13 @@
+package com.personal.projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
